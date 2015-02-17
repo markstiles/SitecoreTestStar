36dc2a7b50493c2d201e1a251fbc38b52baabc30
Mark Stiles <stiles.mark@gmail.com>
2015-02-17 17:37:58 -0500

    stubbing out the extension tests

--- Sitecore.TestStar.SelfTests/ExtensionTests.cs
+++ Sitecore.TestStar.SelfTests/ExtensionTests.cs
@@ -10,23 +10,87 @@ namespace Sitecore.TestStar.SelfTests {
     [TestFixture, Category("Extension Tests")]
     public class ExtensionTests {
         
-		[Test]
-        public void Test() {
-            //TestExtensions.SetProperty(); //x2 one for TestMethod one for Test
-            //TestExtensions.GetProperty(); //x2 one for TestMethod one for ITest
-            //TestExtensions.GetMethod();
-            //TestExtensions.GetMethods();
-			//TestExtensions.GetMethodsByCategory(this TestSuite suite, string category)
-            //TestExtensions.GetFixtures();
-            //TestExtensions.GetAllCategories();
-            //TestExtensions.Categories();
+        #region SitecoreExtensions
 
-			//SitecoreExtensions.GetItemByID
-			//SitecoreExtensions.GetSafeFieldValue
-			//SitecoreExtensions.GetSafeFieldList
-			//SitecoreExtensions.GetSafeFieldBool
-			//SitecoreExtensions.GetSafeDateFieldValue
-			//SitecoreExtensions.ToDateFieldValue
-		}
+        [Test]
+        public void SCExtensions_() {
+            //SitecoreExtensions.GetItemByID
+        }
+
+        [Test]
+        public void SCExtensions_() {
+            //SitecoreExtensions.GetSafeFieldValue
+        }
+        
+        [Test]
+        public void SCExtensions_() {
+            //SitecoreExtensions.GetSafeFieldList
+        }
+        
+        [Test]
+        public void SCExtensions_() {
+            //SitecoreExtensions.GetSafeFieldBool
+        }
+        
+        [Test]
+        public void SCExtensions_() {
+            //SitecoreExtensions.GetSafeDateFieldValue
+        }
+        
+        [Test]
+        public void SCExtensions_() {
+            //SitecoreExtensions.ToDateFieldValue
+        }
+
+        #endregion SitecoreExtensions
+
+        #region TestExtensions
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.SetProperty(); //x2 one for TestMethod one for Test
+        }
+
+        [Test]
+        public void TestExtensions_() {
+
+        }
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.GetProperty(); //x2 one for TestMethod one for ITest
+        }
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.GetMethod();
+        }
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.GetMethods();
+        }
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.GetMethodsByCategory(this TestSuite suite, string category)
+        }
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.GetFixtures();
+        }
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.GetAllCategories();
+        }
+
+        [Test]
+        public void TestExtensions_() {
+            //TestExtensions.Categories();
+        }
+
+        #endregion TestExtensions
     }
 }
